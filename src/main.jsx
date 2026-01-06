6bd643e0b95b9eba0334993596c7375c62daffbe
Adeduro Toluwani <adedurotolu5@gmail.com>
2026-01-06 18:28:09 +0100

    fix: use lowercase import './app.css' to match actual filename

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './App.css'
-import App from './App.jsx'
+import './app.css'
+import App from './app.jsx'
 
 
 createRoot(document.getElementById('root')).render(
